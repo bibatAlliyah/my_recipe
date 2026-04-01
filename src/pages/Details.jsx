e2034039919547f93a22963925584bffd5ed9bcd
Alliyah Myka Bibat <mykabibat@gmail.com>
2026-04-02 01:06:40 +0800

    fixed details, accidentally deleted some stuff there eh sorry

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,49 +1,65 @@
-return (
-  <div style={{ padding: '20px' }}>
-    <div style={{ textAlign: 'left' }}>
-      <button
-        onClick={() => navigate('/')}
+import { useParams, useNavigate } from 'react-router-dom'
+import { useGetMealByIdQuery } from '../redux/apiSlice'
+
+function Details() {
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  //get meal data
+  const { data, isLoading } = useGetMealByIdQuery(id)
+  const meal = data?.meals?.[0]
+
+  if (isLoading) return <p>Loading...</p>
+  if (!meal) return <p>No meal found</p>
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <div style={{ textAlign: 'left' }}>
+        <button
+          onClick={() => navigate('/')}
+          style={{
+            marginBottom: '20px',
+            padding: '8px 12px',
+            cursor: 'pointer',
+          }}
+        >
+          ← Home
+        </button>
+      </div>
+
+      <div
         style={{
-          marginBottom: '20px',
-          padding: '8px 12px',
-          cursor: 'pointer',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginBottom: '20px'
         }}
       >
-        ← Home
-      </button>
-    </div>
-
-    <div
-        style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            marginBottom: '20px'
-        }}
-    >
         <h1>{meal.strMeal}</h1>
 
         <img
-            src={meal.strMealThumb}
-            alt={meal.strMeal}
-            style={{
+          src={meal.strMealThumb}
+          alt={meal.strMeal}
+          style={{
             width: '300px',
             borderRadius: '10px'
-            }}
+          }}
         />
+      </div>
+
+      <div style={{ marginTop: '20px', lineHeight: '1.8', textAlign: 'left' }}>
+        <h3>Instructions</h3>
+
+        {meal.strInstructions
+          ?.split('. ')
+          .map((step, index) => (
+            <p key={index}>
+              <strong>Step {index + 1}:</strong> {step.trim()}.
+            </p>
+          ))}
+      </div>
     </div>
+  )
+}
 
-    <div style={{ marginTop: '20px', lineHeight: '1.8', textAlign: 'left' }}>
-      <h3>Instructions</h3>
-
-      {meal.strInstructions
-        .split('. ')
-        .map((step, index) => (
-          <p key={index}>
-            <strong>Step {index + 1}:</strong> {step.trim()}.
-          </p>
-        ))}
-    </div>
-
-  </div>
-)
+export default Details
